3a2b75a93f76f9220d4de581228bd1b81a0c55a4
JDgoldvox <voxcastjdgoldvox@gmail.com>
2026-06-07 23:38:14 +0100

    add statisticnode

--- fontend/src/pages/Dashboard.jsx
+++ fontend/src/pages/Dashboard.jsx
@@ -1,8 +1,10 @@
-﻿
+﻿import SmallStatisticNode from "@/Components/StatisticNode.jsx";
+
 export default function Dashboard() {
     return (
         <>
             <p> hello world, this is dashboard </p>
+            <SmallStatisticNode statistic="123456789" />
         </>
     )
 }
