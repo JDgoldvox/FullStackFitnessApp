7afeec607a18a30fb4fd369b6b002160b2aec0ea
JDgoldvox <voxcastjdgoldvox@gmail.com>
2026-06-09 22:19:40 +0100

    better nav bar seperation between home logo and links

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -4,23 +4,23 @@ import {NavLink} from 'react-router'
 export default function Navbar() {
     return (
         <>
-            <nav>
+            <nav className="navbar-container">
                 
                 <div className="navbar-logo">
                     <NavLink to="/">FitWiz</NavLink>
                 </div>
                 
-                <ul className="nav-bar">
-                    <li className="nav-bar-item">
+                <ul className="navbar-links">
+                    <li className="navbar-item">
                         <NavLink to="/">Dashboard</NavLink>
                     </li>
-                    <li className="nav-bar-item">
+                    <li className="navbar-item">
                         <NavLink to="/Exercises">Exercises</NavLink>
                     </li>
-                    <li className="nav-bar-item">
+                    <li className="navbar-item">
                         <NavLink to="/Goals">Goals</NavLink>
                     </li>
-                    <li className="nav-bar-item">
+                    <li className="navbar-item">
                         <NavLink to="/Community">Community</NavLink>
                     </li>
                 </ul>
